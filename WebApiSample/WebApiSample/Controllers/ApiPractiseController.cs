c5c231327c187ab3f9ad6aa3693749e78e8fccd5
Mani <manisoft55@gmail.com>
2022-10-11 10:10:06 +0530

    http get sample method

---
+++ WebApiSample/WebApiSample/Controllers/ApiPractiseController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    
+    public class ApiPractiseController : ControllerBase
+    {
+        //[HttpGet]
+        //public string GetString()
+        //{
+        //    return "Output";
+        //}
+
+        [HttpGet]
+        public string InputData(string inputVariable)
+        {
+            return $"Your input data is {inputVariable}";
+        }
+    }
+}
