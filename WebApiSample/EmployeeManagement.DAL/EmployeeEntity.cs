1dd0c80d0a5aa355d01b0a6852b9a259e69f5d26
Mani <manisoft55@gmail.com>
2022-11-03 08:03:34 +0530

    Ado.net sample code

---
+++ WebApiSample/EmployeeManagement.DAL/EmployeeEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EmployeeManagement.DAL
+{
+    public class EmployeeEntity
+    {
+        public DataSet GetAllEmployee()
+        {
+            DataSet ds = new DataSet();
+            string strConString = @"Data Source=MSP-LAPTOP;Initial Catalog=InstituteCmd;Integrated Security=True";
+            using (SqlConnection con = new SqlConnection(strConString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from employees", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(ds);
+            }
+            return ds;
+        }
+    }
+}
